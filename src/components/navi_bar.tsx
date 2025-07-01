4d683f2e8d0d245c868e696c5aed0cc7f29fc545
ingonzales-strat <i_isaiahnikolo.gonzales@stratpoint.com>
2025-07-02 00:50:23 +0800

    feat: :lipstick: addition of footer icons as well as integrated navi bar in layout

--- src/components/navi_bar.tsx
+++ src/components/navi_bar.tsx
@@ -2,11 +2,19 @@ import Link from 'next/link';
 
 export default function NavBar() {
   return (
-    <nav className="flex gap-6 bg-gray-800 p-4 text-white font-semibold">
-      <Link href="/">Home</Link>
-      <Link href="/projects"> My Projects</Link>
-      <Link href="/about_me">About Me</Link>
-      <Link href="/contact_me">Contact Me</Link>
+    <nav className="flex gap-6  p-5 text-white ">
+      <MenuItem name='Home' href='/'/>
+      <MenuItem name='My Projects' href='/projects'/>
+      <MenuItem name='About Me' href='/about_me'/>
+      <MenuItem name='Contact Me' href='/contact_me'/>
     </nav>
   );
+}
+
+function MenuItem({name, href}: { name: string; href: string }){
+    return <div><p className=" relative group inline-block">
+      <Link href={href} className='relative z-10 group-hover:text-black transition-colors'>{name}</Link>
+      <span className="absolute px-2 left-0 -bottom-0 w-full h-0.5 bg-white -z-10 group-hover:h-full group-hover:transition-all"></span>
+      </p>
+      </div>
 }
