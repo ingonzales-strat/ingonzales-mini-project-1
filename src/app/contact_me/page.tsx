74ccb2ac8d3921ac1e18587d9de067a17038d30e
ingonzales-strat <i_isaiahnikolo.gonzales@stratpoint.com>
2025-07-02 01:16:59 +0800

    Revert "feat(page content): :sparkles: Gallery page"
    This reverts commit 4a78ac4743d16b8978c9dc36c9ec051993ab22e9.

--- src/app/contact_me/page.tsx
+++ src/app/contact_me/page.tsx
@@ -5,27 +5,21 @@ import { IconBrandGithub,IconBrandLinkedin, IconMailbox } from "@tabler/icons-re
 
 
 export default function ContactMePage() {
-  return <div className="grid grid-rows-[20px_1fr_auto] items-center justify-items-center  p-8 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-    <div>
-      <h1 className="font-bold text-3xl">
-        <center>I&apos;d love to hear from you!</center></h1>
-      <h2 className="font-medium text-lg">
-        <center>You can reach me via...</center>
-      </h2>
-    </div>
+  return <div><h1><center>I&apos;d love to hear from you!</center></h1>
+    <h2><center>You can reach me via...</center></h2>
     <div className="flex flex-wrap justify-center gap-2 md:flex-row">
     <a href="mailto:isaiahnikologonzales@outlook.com">
-      <Button variant="outline" size="sm" className="hover:bg-blue-500">
-      <IconMailbox/> Email
+      <Button variant="outline" size="sm" >
+      <IconMailbox/> EMail
       </Button>
     </a>
     <a href="https://www.linkedin.com/in/inikologon/" target="_blank"rel="noopener noreferrer">
-      <Button variant="outline" size="sm" className="hover:bg-blue-500">
+      <Button variant="outline" size="sm">
         <IconBrandLinkedin /> LinkedIn
       </Button>
     </a>
     <a href="https://github.com/Koniiro" target="_blank" rel="noopener noreferrer">
-      <Button variant="outline" size="sm" className="hover:bg-blue-500">
+      <Button variant="outline" size="sm">
         <IconBrandGithub /> GitHub
       </Button>
     </a>
--- src/app/gallery/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function gallery_page(){
-    
-}
