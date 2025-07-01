4a78ac4743d16b8978c9dc36c9ec051993ab22e9
ingonzales-strat <i_isaiahnikolo.gonzales@stratpoint.com>
2025-07-02 01:15:54 +0800

    feat(page content): :sparkles: Gallery page

--- src/app/about_me/page.tsx
+++ src/app/about_me/page.tsx
@@ -2,6 +2,10 @@ export default function AboutMePage() {
   return <div><h1 className="text-3xl font-bold underline"><center>Hi I&apos;m Niko!</center></h1>
   <p>I am currently a graduating BS Computer Science Student from UP Diliman. 
     I&apos;m generally interesting in web and app development. Through out my time as an undergraduate I have 
-    also poked around with ML and theoretical computing.</p></div>
+    also poked around with ML and theoretical computing.</p>
+    <h2>What am I passionate about?</h2>
+    
+    
+    </div>
   
 }
