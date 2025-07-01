44ee06faeadfb4053f0d343090e749309bbd0729
ingonzales-strat <i_isaiahnikolo.gonzales@stratpoint.com>
2025-07-02 01:19:48 +0800

    refactor(page content): Updated styling for contact page

--- src/app/contact_me/page.tsx
+++ src/app/contact_me/page.tsx
@@ -5,21 +5,27 @@ import { IconBrandGithub,IconBrandLinkedin, IconMailbox } from "@tabler/icons-re
 
 
 export default function ContactMePage() {
-  return <div><h1><center>I&apos;d love to hear from you!</center></h1>
-    <h2><center>You can reach me via...</center></h2>
+  return <div className="grid grid-rows-[20px_1fr_auto] items-center justify-items-center  p-8 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div>
+      <h1 className="font-bold text-3xl">
+        <center>I&apos;d love to hear from you!</center></h1>
+      <h2 className="font-medium text-lg">
+        <center>You can reach me via...</center>
+      </h2>
+    </div>
     <div className="flex flex-wrap justify-center gap-2 md:flex-row">
     <a href="mailto:isaiahnikologonzales@outlook.com">
-      <Button variant="outline" size="sm" >
-      <IconMailbox/> EMail
+      <Button variant="outline" size="sm" className="hover:bg-blue-500">
+      <IconMailbox/> Email
       </Button>
     </a>
     <a href="https://www.linkedin.com/in/inikologon/" target="_blank"rel="noopener noreferrer">
-      <Button variant="outline" size="sm">
+      <Button variant="outline" size="sm" className="hover:bg-blue-500">
         <IconBrandLinkedin /> LinkedIn
       </Button>
     </a>
     <a href="https://github.com/Koniiro" target="_blank" rel="noopener noreferrer">
-      <Button variant="outline" size="sm">
+      <Button variant="outline" size="sm" className="hover:bg-blue-500">
         <IconBrandGithub /> GitHub
       </Button>
     </a>
