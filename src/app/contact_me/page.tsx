f06492c6fc6884464b29157725f483ea750671e9
ingonzales-strat <i_isaiahnikolo.gonzales@stratpoint.com>
2025-07-02 15:17:39 +0800

    feat(page content): :art: adjusted contact page format

--- src/app/contact_me/page.tsx
+++ src/app/contact_me/page.tsx
@@ -1,25 +1,27 @@
 import { Button } from "@/components/ui/button"
-import { IconBrandGithub,IconBrandLinkedin, IconMailbox } from "@tabler/icons-react"
+import { IconBrandGithub,IconBrandLinkedin } from "@tabler/icons-react"
 
 
 
 
 export default function ContactMePage() {
-  return <div className="grid grid-rows-[20px_1fr_auto] items-center justify-items-center  p-8 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-    <div>
-      <h1 className="font-bold text-3xl">
-        <center>I&apos;d love to hear from you!</center></h1>
-      <h2 className="font-medium text-lg">
-        <center>You can reach me via...</center>
-      </h2>
-    </div>
-    <div className="flex flex-wrap justify-center gap-2 md:flex-row">
-    <a href="mailto:isaiahnikologonzales@outlook.com">
-      <Button variant="outline" size="sm" className="hover:bg-blue-500">
-      <IconMailbox/> Email
-      </Button>
-    </a>
-    <a href="https://www.linkedin.com/in/inikologon/" target="_blank"rel="noopener noreferrer">
+  return <div className="grid  items-center justify-items-center p-8 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+  <div className=" flex flex-col items-center text-center space-y-2">
+    <h1 className="font-bold text-7xl">Wanna chat?</h1>
+    <h2 className="font-medium text-2xl">You can reach me at...</h2>
+    <ul>
+      <li>
+        <div><p className="text-lg relative group inline-block">
+      <a href="mailto:isaiahnikologonzales@outlook.com" className='relative z-10 group-hover:text-black transition-colors'>isaiahnikologonzales@outlook.com</a>
+      <span className="absolute px-2 left-0 -bottom-0 w-full h-0.5 bg-white -z-10 group-hover:h-full group-hover:transition-all"></span>
+      </p>
+      </div>
+      </li>
+    </ul>
+  </div>
+
+  <div className="flex flex-wrap justify-center gap-2 md:flex-row">
+    <a href="https://www.linkedin.com/in/inikologon/" target="_blank" rel="noopener noreferrer">
       <Button variant="outline" size="sm" className="hover:bg-blue-500">
         <IconBrandLinkedin /> LinkedIn
       </Button>
@@ -29,7 +31,7 @@ export default function ContactMePage() {
         <IconBrandGithub /> GitHub
       </Button>
     </a>
-   </div>
-    
-   </div>
+  </div>
+</div>
+
 }
