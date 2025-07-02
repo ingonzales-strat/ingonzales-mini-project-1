d03249bb7d8a8163951bde8b99fbc14e083eae0b
ingonzales-strat <i_isaiahnikolo.gonzales@stratpoint.com>
2025-07-03 01:04:23 +0800

    refactor(navigation): :lipstick: added hover animation
    Added a hover animation that changes from filled to outline and grows the icon on hover

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,18 +1,24 @@
-import { IconBrandGithubFilled,IconBrandLinkedinFilled, IconMailbox } from "@tabler/icons-react"
+import { IconBrandGithubFilled,IconBrandGithub,IconBrandLinkedinFilled, IconMail, IconMailFilled, IconBrandLinkedin } from "@tabler/icons-react"
 export default function MyFooter() {
   return (
     <footer className="row-start-3 flex gap-[24px] p-8 flex-wrap items-center justify-center">
-          <a className="flex items-center gap-2 hover:underline hover:underline-offset-4"href="mailto:isaiahnikologonzales@outlook.com"
+          <a className="group flex items-center gap-2 "href="mailto:isaiahnikologonzales@outlook.com"
             target="_blank" rel="noopener noreferrer">
-              <IconMailbox/>
+              <IconMailFilled className="group-hover:hidden"/>
+              <IconMail stroke={1.75} className="hidden group-hover:scale-125 group-hover:inline  transition-transform group-hover"/>
+
           </a>
-          <a className="flex items-center gap-2 hover:underline hover:underline-offset-4"href="https://www.linkedin.com/in/inikologon/"
+          <a className="group flex items-center gap-2 "href="https://www.linkedin.com/in/inikologon/"
             target="_blank" rel="noopener noreferrer">
-              <IconBrandLinkedinFilled/>
+              <IconBrandLinkedinFilled className="group-hover:hidden"/>
+              <IconBrandLinkedin stroke={1.75} className="hidden group-hover:scale-125 group-hover:inline  transition-transform group-hover"/>
           </a>
-          <a className="flex items-center gap-2 hover:underline hover:underline-offset-4"href="https://github.com/Koniiro"
+          <a className="group flex items-center gap-2 "href="https://github.com/Koniiro"
             target="_blank" rel="noopener noreferrer">
-              <IconBrandGithubFilled/>
+              <IconBrandGithubFilled  className="group-hover:hidden"/>
+              <IconBrandGithub stroke={1.75} className="hidden group-hover:scale-125 group-hover:inline  transition-transform group-hover"/>
+
+              
           </a>
         </footer>
   );
