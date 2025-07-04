6d8a0c2aaea2c09fc0d244606055ecaddd79c3c5
ingonzales-strat <i_isaiahnikolo.gonzales@stratpoint.com>
2025-07-04 14:53:34 +0800

    fix(UI style): updated wipe up link component

--- src/components/wipeUpLink.tsx
+++ src/components/wipeUpLink.tsx
@@ -4,7 +4,7 @@ export default function WipeUpLink({text, href}: { text: string; href: string })
   return (
     <div>
         <p className="px-0.5  relative group inline-block">
-        <Link href={href} className='relative z-10 text-white group-hover:text-black transition-colors'>{text}</Link>
+        <Link href={href} className='relative z-10 text-white group-hover:text-black transition-colors underline decoration-blue-500 decoration-1 underline-offset-4'>{text}</Link>
         <span className="absolute px-2 left-0 -bottom-0 w-full h-0.5 bg-blue-500 -z-10 group-hover:h-full group-hover:transition-all"></span>
         </p>
     </div>
@@ -15,7 +15,7 @@ export  function WipeUpLinkExt({text, href}: { text: string; href: string }) {
   return (
     <div>
         <p className="px-0.5  relative group inline-block">
-        <a href={href} className='relative z-10 text-white group-hover:text-black transition-colors'>{text}</a>
+        <a href={href} className='relative z-10 text-white group-hover:text-black transition-colors underline decoration-blue-500 decoration-1 underline-offset-4'>{text}</a>
         <span className="absolute px-2 left-0 -bottom-0 w-full h-0.5 bg-blue-500 -z-10 group-hover:h-full group-hover:transition-all"></span>
         </p>
     </div>
